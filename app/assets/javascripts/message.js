2b6090315971419ecb654e0a4052ea90e97fac4d
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-04-06 14:49:24 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,9 +41,6 @@ $(function(){
    };
  }
 
-
-
-
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -69,31 +66,21 @@ $('#new_message').on('submit', function(e){
   $('.submit-btn').prop('disabled', false);
 })
 })
-
-
-
 var reloadMessages = function() {
-  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
   var last_message_id = $('.messages__post:last').data("data-message-id");
   console.log(last_message_id)
   $.ajax({
-    //ルーティングで設定した通りのURLを指定
     url: "api/messages",
-    //ルーティングで設定した通りhttpメソッドをgetに指定
     type: 'get',
     dataType: 'json',
-    //dataオプションでリクエストに値を含める
     data: {id: last_message_id}
   })
   .done(function(messages) {
     if (messages.length !== 0) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     }
@@ -103,7 +90,7 @@ var reloadMessages = function() {
   });
 };
 
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
+  // if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  //   setInterval(reloadMessages, 7000);
+  // }
 });
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,5 @@
+Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
+Date::DATE_FORMATS[:date] = "%m/%d"
+DateTime::DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
+Time::DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
+DateTime::DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
